03cacabf72d42e7ebe5610897433c898ad440b69
alexaaa-a <144144005+alexaaa-a@users.noreply.github.com>
2023-10-18 18:46:07 +0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,96 @@
+import telebot
+from telebot import custom_filters
+from telebot import StateMemoryStorage
+from telebot.handler_backends import StatesGroup, State
+
+state_storage = StateMemoryStorage()
+
+bot = telebot.TeleBot("6490209375:AAE43hmecxmuIO8qc9gQRyA2qnfVdXY8aAk",
+                      state_storage=state_storage, parse_mode='Markdown')
+
+
+class PollState(StatesGroup):
+    name = State()
+    age = State()
+
+
+class HelpState(StatesGroup):
+    wait_text = State()
+
+
+text_poll = "Проходим опрос 💅"  # Можно менять текст
+text_button_1 = "Сайт Умскул"  # Можно менять текст
+text_button_2 = "Канал Умскул"  # Можно менять текст
+text_button_3 = "Анекдот (можно будет скопировать)"  # Можно менять текст
+
+menu_keyboard = telebot.types.ReplyKeyboardMarkup(one_time_keyboard=True, resize_keyboard=True)
+menu_keyboard.add(
+    telebot.types.KeyboardButton(
+        text_poll,
+    )
+)
+menu_keyboard.add(
+    telebot.types.KeyboardButton(
+        text_button_1,
+    )
+)
+
+menu_keyboard.add(
+    telebot.types.KeyboardButton(
+        text_button_2,
+    ),
+    telebot.types.KeyboardButton(
+        text_button_3,
+    )
+)
+
+
+@bot.message_handler(state="*", commands=['start'])
+def start_ex(message):
+    bot.send_message(
+        message.chat.id,
+        'Привет-привет! Чем я смогу *повеселить* Вас сегодня?',  # Можно менять текст
+        reply_markup=menu_keyboard)
+
+
+@bot.message_handler(func=lambda message: text_poll == message.text)
+def first(message):
+    bot.send_message(message.chat.id, 'Хороший выбор! Назовите _Ваше_ имя?')  # Можно менять текст
+    bot.set_state(message.from_user.id, PollState.name, message.chat.id)
+
+
+@bot.message_handler(state=PollState.name)
+def name(message):
+    with bot.retrieve_data(message.from_user.id, message.chat.id) as data:
+        data['name'] = message.text
+    bot.send_message(message.chat.id, 'Шикарное имя! _Ваш_ возраст?')  # Можно менять текст
+    bot.set_state(message.from_user.id, PollState.age, message.chat.id)
+
+
+@bot.message_handler(state=PollState.age)
+def age(message):
+    with bot.retrieve_data(message.from_user.id, message.chat.id) as data:
+        data['age'] = message.text
+    bot.send_message(message.chat.id, 'Спасибо за регистрацию!', reply_markup=menu_keyboard)  # Можно менять текст
+    bot.delete_state(message.from_user.id, message.chat.id)
+
+
+@bot.message_handler(func=lambda message: text_button_1 == message.text)
+def help_command(message):
+    bot.send_message(message.chat.id, "[Сайт Умскул](https://umschool.net/), Там вам помахают Александр и Анастасия <3", reply_markup=menu_keyboard)  # Можно менять текст
+
+
+@bot.message_handler(func=lambda message: text_button_2 == message.text)
+def help_command(message):
+    bot.send_message(message.chat.id, "[Канал Умскул](https://www.youtube.com/channel/UCggA5GaSPVyjsqomtwR5ufw), там есть *много* интересного", reply_markup=menu_keyboard)  # Можно менять текст
+
+
+@bot.message_handler(func=lambda message: text_button_3 == message.text)
+def help_command(message):
+    bot.send_message(message.chat.id, "Какой пиджак носят курящие люди? Смокинг", reply_markup=menu_keyboard)  # Можно менять текст
+
+
+bot.add_custom_filter(custom_filters.StateFilter(bot))
+bot.add_custom_filter(custom_filters.TextMatchFilter())
+
+bot.infinity_polling()
